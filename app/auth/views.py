29583ce2e973308d9b1670668dad79ae50a0c315
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-04 23:50:41 +0300

    add auth route in for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,1 +1,11 @@
 from flask import render_template,request,redirect,url_for,abort
+
+@auth.route('/login')
+def login():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    
+    return render_template('login.html')    
