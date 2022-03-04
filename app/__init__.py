817a553be029cdeb8da24e0a469ec267b7f00069
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-04 22:07:48 +0300

    add auth directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 
@@ -15,5 +17,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
 
     # Will add the views and forms
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,11 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
-@main.route('/index')
+@main.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
     
-    message = 'Hello World' 
-    return render_template('index.html',message = message)
+    
+    return render_template('index.html')
