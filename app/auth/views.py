9368453fe4a1733adbf68a61c74e66e7afa28218
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-04 23:55:01 +0300

    add auth route for logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,3 +19,13 @@ def signup():
     
     
     return render_template('signup.html')        
+
+@auth.route('/logout')
+def logout():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    
+    return redirect(url_for'main.index')            
