c2af00ce267d5c0d13af55e02692e9a97205c9f3
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-05 11:18:22 +0300

    check user in database

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,8 @@
 from flask import render_template,redirect,url_for
 from . import auth
+from .models import User
+from . import db
+
 @auth.route('/login')
 def login():
 
@@ -18,7 +21,18 @@ def signup():
     '''
     
     
-    return render_template('auth/signup.html')        
+    return render_template('auth/signup.html')     
+
+
+@auth.route('/signup',methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')  
+    
+    user = User.query.filter_by(email=email).first()
+     
+
 
 @auth.route('/logout')
 def logout():
