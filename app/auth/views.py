1da9ee17d241d14aa8f320d535bb16f658bad6dc
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-05 12:12:00 +0300

    login functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,18 +2,35 @@ from flask import render_template,redirect,url_for, request, flash
 from . import auth
 from ..models import User
 from .. import db
+from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @auth.route('/login')
 def login():
-
+  
     '''
     View root page function that returns the index page and its data
     '''
     
     
     return render_template('auth/login.html')    
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user and not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+
+    return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
 def signup():
