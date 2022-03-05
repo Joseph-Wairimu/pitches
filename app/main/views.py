3383475a7db8f84dc7782ee1d7728f5929f6b5ee
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-05 13:33:04 +0300

    adding forms to our main blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -6,13 +6,26 @@ from . import main
 def index():
     return render_template('index.html')
 
-@main.route('/profile')
-@login_required
-def profile():
+# @main.route('/profile')
+# def profile():
 
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+    
+    
+#     return render_template('profile/profile.html')    
+
+
+
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
     '''
     View root page function that returns the index page and its data
     '''
     
     
-    return render_template('profile/profile.html',username=current_user.username)    
+    return render_template('profile/profile.html',user = user)    
+
