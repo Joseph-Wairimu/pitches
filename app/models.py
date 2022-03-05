698a1d3065187d448397a7ed50648a6aa1fd8113
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-05 20:43:24 +0300

    add post to database

--- app/models.py
+++ app/models.py
@@ -14,3 +14,21 @@ class User(UserMixin,db.Model):
 
      
 class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer,primary_key=True)
+    post = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    vote_count = db.Column(db.Integer)
+    added_date = db.Column(db.DateTime,default=datetime.utcnow)
+    author = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"    
