c8c7150e00418e5a087894927f4652c390ccf931
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-05 13:42:05 +0300

    update file for our profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,request,redirect,url_for,abort
-
+from .forms import UpdateProfile
 from flask_login import login_required, current_user
 from . import main
 @main.route('/')
@@ -29,3 +29,21 @@ def profile(uname):
     
     return render_template('profile/profile.html',user = user)    
 
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
