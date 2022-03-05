43ab87f26575033deae3dcad5ec9b89db5ce31a2
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-05 13:11:26 +0300

    user profile

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(UserMixin,db.Model):
     email=db.Column(db.String(100),unique = True)
     password =db.Column(db.String(100))
     username = db.Column(db.String(1000))
-
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     def __repr__(self):
         return f'User {self.username}'
