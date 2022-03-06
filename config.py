ed2d8d5074bcf9ed2c9a834aba7ca749291a74a6
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-06 15:05:46 +0300

    edit local host on config

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class ProdConfig(Config):
     
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost:5433/pitch'
     DEBUG = True  
     
     
