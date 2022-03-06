069655874ba661c42e2d85e9e8b0f5542093cd79
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-06 17:24:37 +0300

    config update

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,7 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://moringa:Access@localhost/pitch'
 
     bootstrap.init_app(app)
     db.init_app(app)
