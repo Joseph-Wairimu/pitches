260dd1ff388372cd77c3543875a10037065c1e06
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-06 13:20:58 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
